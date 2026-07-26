29f6d5ae6e8664339220226660b95a22fa07632f
dIB59 <ibrahim@ebs.net.pk>
2026-07-26 15:24:46 +0200

    [Enhancement] Add nearest neighbor scaling for images via `ImagePlugin` in app initialization
    Signed-off-by: dIB59 <ibrahim@ebs.net.pk>

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ use bevy_simple_subsecond_system::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "RPG Raid Shop".into(),
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "RPG Raid Shop".into(),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_plugins(SimpleSubsecondPlugin::default())
         .add_plugins(PlayerPlugin)
         .run();
